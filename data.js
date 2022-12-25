c44164ee70d014c33beda4a5389c76c2825f4580
snowdonstudio <63182046+snowdonstudio@users.noreply.github.com>
2022-12-25 14:17:20 +0530

    First commit

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20221221_182738_00_012-1",
+      "name": "Greatly Hotels Test",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 3.1218813003301786,
+        "pitch": 0.03739041139116672,
+        "fov": 1.1828363345513648
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Greatly Hotels",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
